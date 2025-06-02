6c740ca26902ec5f10cab0f93c829c86c8838250
luozhifeng <228539650@qq.com>
2025-06-02 21:42:07 +0800

    hot-fix 1 commit lzf

--- src/main/java/com/lzf/git/GitTest.java
+++ src/main/java/com/lzf/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!!!!");
         System.out.println("hello git222!!!!");
         System.out.println("hello git333!!!!");
+        System.out.println("hello git44444!!!!");
     }
 }
